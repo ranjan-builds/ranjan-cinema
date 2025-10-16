3d1aa2378dd213931a2d9010c8281919e5b0f9c5
ranjan-builds <ranjankashyap6475@gmail.com>
2025-10-17 00:25:29 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Movie from "./pages/movie";
